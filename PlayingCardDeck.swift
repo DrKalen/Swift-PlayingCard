e1769a9dcae14659c3c47d4dc4e1ee49bc6f3db0
Kalen Hammann <kalen7@gmail.com>
2018-07-22 17:28:18 -0400

    Initial Commit: Draw Cards and DeckOfCards

---
+++ PlayingCardDeck.swift
@@ -0,0 +1,42 @@
+//
+//  PlayingCardDeck.swift
+//  PlayingCard
+//
+//  Created by Kalen Hammann on 7/17/18.
+//  Copyright © 2018 Human Family. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCardDeck
+{
+    private(set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))     // abs is "absolute value of"
+        } else {
+            return 0
+        }
+    }
+}
